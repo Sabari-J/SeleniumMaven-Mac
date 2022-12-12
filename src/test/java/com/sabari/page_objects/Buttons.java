d3d7cd5915e82e6a49ff2a00c0b64635e1ea38ad
Sabari <sabari.sun7@gmail.com>
2022-12-12 08:01:21 +0530

    Home_1

--- src/test/java/com/sabari/page_objects/Buttons.java
+++ src/test/java/com/sabari/page_objects/Buttons.java
@@ -8,35 +8,21 @@ import base.login;
 
 public class Buttons extends login {
 
-	//private WebElement buttons;
-
+	
 	Elements elemnts = new Elements();
 
-//	// Tempsolutn
-	//public WebElement elementsTab;
 
-//	@Test
-//	public void clickOnElements() throws InterruptedException {
-//
-//		System.out.println("***");
-//		Thread.sleep(3000);
-//		elementsTab = driver.findElement(By.xpath("//h5[.= 'Elements']"));
-//		System.out.println(elementsTab.isDisplayed());
-//
-//		scrollToElement(elementsTab);
-//		elementsTab.click();
-//
-//	}
 
-	@Test
+	@Test(priority=0)
 	public void clickOnButtons() throws InterruptedException {
 
 		elemnts.clickOnElements();
-		Thread.sleep(3000);
+		//Thread.sleep(3000);
 		System.out.println("++++++");
 
-		WebElement buttons = driver.findElement(By.xpath("//span[.='Buttons']"));
-		clickableElement(buttons, 10);
+		WebElement buttons = driver.findElement(By.xpath("//span[(.='Buttons')]"));
+		buttons.click();
+		//clickableElement(buttons, 10);
 
 	}
 
