6851775474b526a9e928052beb1855725b110e7c
Sabari <sabari.sun7@gmail.com>
2022-12-12 08:01:21 +0530

    Home_1

--- src/test/java/com/sabari/page_objects/Elements.java
+++ src/test/java/com/sabari/page_objects/Elements.java
@@ -10,19 +10,20 @@ public class Elements extends login {
 
 	private WebElement textBox;
 
-	@Test
+	@Test(priority=0)
 	public void clickOnElements() throws InterruptedException {
 
 		System.out.println("***");
-		Thread.sleep(3000);
+		//Thread.sleep(3000);
 		WebElement elementsTab = driver.findElement(By.xpath("//h5[.= 'Elements']"));
 		System.out.println(elementsTab.isDisplayed());
 		
 		scrollToElement(elementsTab);
 		elementsTab.click();
+		System.out.println("++++++");
 	}
 
-	@Test
+	@Test(priority=1)
 	public void clickOnTextBox() {
 
 		textBox = driver.findElement(By.xpath("//span[.='Text Box']"));
